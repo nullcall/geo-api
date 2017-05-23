4eee97539d535e95d1c93bf5fe0b1650682f362f
Zuber Sheikh <engineer.nova@gmail.com>
2017-05-23 19:17:29 +0530

    Final Deploy.

--- src/MapAPI.php
+++ src/MapAPI.php
@@ -151,29 +151,5 @@ class MapAPI {
 		return  $g;
 	}
 }
-####################################################################
-
-/// variables 
-	$lat = '27.453731';
-	$long ='74.623032';
-	$coDis = array(
-				'0' => array('lat' => '18.92237167', 'long' => '72.827865' ),
-				'1' => array('lat' => '18.94096833', 'long' => '72.83850833' ),
-				// '2' => array('lat' => '28.463731', 'long' => '74.64032' ),
-				// '3' => array('lat' => '28.50731', 'long' => '75.66032' ),
-				);
-
-	echo "<PRE>";
-
-///// Done //////
-
-/// Fetch Address for given coordinates ///
-	$gc = mapAPI::SetLatLong($lat,$long);
-	print_r($gc->addFetch());
-/// Call Object to init the value in main Class ////////
-	$gc = mapAPI::setDisctance($coDis,$unit = 'k');
-	print_r($gc->calDistance());
-	echo "<br />";
-/// Call Object to for direction API
-	$direction = mapAPI::setDirection($coDis);
-	print_r($direction->getDirection());
+###################### END of GEO API ##############################################
+?>
